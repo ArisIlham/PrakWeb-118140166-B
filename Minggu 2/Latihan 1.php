d627dc79f241b152ae026164baa5833b4b56e3e0
ArisIlham <64519295+ArisIlham@users.noreply.github.com>
2020-11-15 21:36:12 +0700

    Minggu 2

---
+++ Minggu 2/Latihan 1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Latihan 1</title>
+    </head>
+<body>
+    
+    <?php 
+
+	echo "Bilangan 1 = ".$a = 20; echo "<br>";
+	echo "Bilangan 2 = ".$b = 5; echo "<br><br>";
+    
+	echo "PENJUMLAHAN <br>";
+	echo "Operator : + <br>";
+	$operator = $a + $b;
+	echo "Hasil : ".$operator."<br><br>";
+
+	echo "PENGURANGAN <br>";
+	echo "Operator : - <br>";
+	$operator = $a - $b;
+	echo "Hasil : ".$operator."<br><br>";
+
+	echo "PERKALIAN <br>";
+	echo "Operator : * <br>";
+	$operator = $a * $b;
+	echo "Hasil : ".$operator."<br><br>";
+
+	echo "PEMBAGIAN <br>";
+	echo "Operator : / <br>";
+	$operator = $a / $b;
+	echo "Hasil : ".$operator."<br><br>";
+
+	echo "MODULUS <br>";
+	echo "Operator : % <br>";
+	$operator = $a % $b;
+	echo "Hasil : ".$operator."<br><br>";
+     ?>
+ </body>
+ </html>
